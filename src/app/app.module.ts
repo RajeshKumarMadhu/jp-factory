84056eb801199093c7b7de0d9f06acb8672c4571
Rajesh Madhu <rajeshmadhu@Rajeshs-MBP.home>
2017-04-12 00:31:40 -0400

    Code Base for the future use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+const appRoutes: Routes = [
+  {path: '', component: AppHomeComponent},
+  { path: 'app-home', component: AppHomeComponent},
+  { path: '**', component: PageNotFoundComponent }
+];
+
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { AppHomeComponent } from './app-home/app-home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
