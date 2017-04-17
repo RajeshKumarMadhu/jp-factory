cd2a1cf5297df9ca507ea7c22a5816e422c13220
RajeshMadhu <Seyalfactory@gmail.com>
2017-04-17 15:13:18 -0400

    Adding report cards functionality for the store owner credits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ const appRoutes: Routes = [
   {path: '', component: AppHomeComponent},
   { path: 'app-home', component: AppHomeComponent},
   { path: 'order-summary', component: OrderSummaryComponent},
-  { path: '**', component: PageNotFoundComponent }
+  { path: 'report-cards', component: ReportCardsComponent},
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 import { RouterModule, Routes } from '@angular/router';
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
 import { AppHomeComponent } from './app-home/app-home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { ReportCardsComponent } from './report-cards/report-cards.component';
 
 
 
@@ -24,7 +26,8 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
     AppComponent,
     AppHomeComponent,
     PageNotFoundComponent,
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    ReportCardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/report-cards/report-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-cards',
+  templateUrl: './report-cards.component.html',
+  styleUrls: ['./report-cards.component.css']
+})
+export class ReportCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
