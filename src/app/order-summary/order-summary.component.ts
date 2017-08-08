a0ebd1c829771a8d3b7986bcc046d805de76e3db
RajeshMadhu <Seyalfactory@gmail.com>
2017-08-07 23:37:03 -0400

    Changes for tax percentage and invoice number.

--- src/app/order-summary/order-summary.component.ts
+++ src/app/order-summary/order-summary.component.ts
@@ -9,8 +9,9 @@ export class OrderSummaryComponent implements OnInit {
 
   constructor() { }
   orderdata = '';
-  taxData = '';
-  totalData = '';
+  taxData = {cgstTaxPercentile : 9.0,sgstTaxPercentile: 9.0 ,cgstTaxAmt:0.0,sgstTaxAmt:0.0};
+  totalData = {subtotal : 0.0,total: 0.0 ,discount:0.0};
+
   ngOnInit() {
       this.orderdata = JSON.parse(sessionStorage.getItem('orderDetails'));
       this.taxData = JSON.parse(sessionStorage.getItem('taxinfo'));
